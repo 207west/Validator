87befb7a8c4b3ce0aa252fc1ab3c39bd1f1ce8e5
Adam Drago <atdrago@gmail.com>
2012-09-05 11:05:34 -0400

    fixed date in header comments

--- Validator.js
+++ Validator.js
@@ -6,7 +6,7 @@
  * 12:34 MicroTechnologies
  * http://12:34micro.com
  *
- * Date: Mon June 29 2012
+ * Date: Wed September 9th 2012
  *
  * Requirements: 
  *					jquery.js, underscore.js, underscore.string.js, bootstrap.js
